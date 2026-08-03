44b31701417431f361cf312a032709ab6e7171b5
Paul <elrobtossohn@gmail.com>
2026-08-04 00:11:52 +0200

    feat: remark-gfm dependency for markdown rendering

--- src/renderer/src/components/Markdown.jsx
+++ src/renderer/src/components/Markdown.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export default function Markdown({ text, className }) {
     return (
         <div className={`markdown-body${className ? ` ${className}` : ''}`}>
-            <ReactMarkdown>{text}</ReactMarkdown>
+            <ReactMarkdown
+                remarkPlugins={[remarkGfm]}
+                components={{
+                    a: ({ href, children }) => (
+                        <a href={href}
+                           onClick={(e) => {
+                               e.preventDefault();
+                               console.log('Opening link:', href);
+                               if (href) {
+                                   console.log(window.api)
+                                   console.log('Opening link2:', href);
+                                   window.api.window.openExternal(href);
+                               }
+                           }}>
+                            {children}
+                        </a>
+                    )}}>
+                {text}
+            </ReactMarkdown>
         </div>
     );
 }
